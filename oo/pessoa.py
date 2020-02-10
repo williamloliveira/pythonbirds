4b4da972d1f937bbe8fea6cf01bc572ccbcd679e
William Lino Oliveira <william.loliveira@hotmail.com>
2020-02-10 16:26:00 -0300

    Melhorias

--- oo/pessoa.py
+++ oo/pessoa.py
@@ -1,7 +1,8 @@
 class Pessoa:
-    def __init__(self, nome = None, idade = None):
+    def __init__(self, *filhos, nome = None, idade = 35):
         self.nome = nome
         self.idade = idade
+        self.filhos = list(filhos)
 
     def cumprimentar(self):
         return f'Olá {id(self)}'
